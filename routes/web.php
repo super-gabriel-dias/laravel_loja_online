79dcb5f1e6772401d8cf2879b279725182e8fe28
gabriel <gabrielmyaga@gmail.com>
2023-11-22 19:52:10 -0300

    feat:new Routes added routes for product's Cart, admin Dashboard, Login forms. fixed the older routes for the products controller, now working with the new site controller.

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,11 @@
 <?php
 
+use App\Http\Controllers\CarrinhoController;
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\LoginController;
 use App\Http\Controllers\ProdutoController;
+use App\Http\Controllers\SiteController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,6 +19,37 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', [ProdutoController::class, 'home']);
+Route::resource('users', UserController::class);
 
-Route::get('/sobre-nos', [ProdutoController::class, 'sobre_nos']);
+
+
+Route::get('/', [SiteController::class, 'home'])->name('site.home');
+
+Route::get('/sobre-nos', [SiteController::class, 'sobre_nos'])->name('site.sobre_nos');
+
+Route::get('/produtos', [SiteController::class, 'produtos'])->name('site.produtos');
+
+Route::get('/produtos/{slug}', [SiteController::class, 'details'])->name('site.details');
+
+Route::get('/categoria/{id}', [SiteController::class, 'categoria'])->name('site.categoria');
+
+Route::get('/carrinho', [CarrinhoController::class, 'carrinhoLista'])->name('site.carrinho');
+
+Route::post('/carrinho', [CarrinhoController::class, 'adicionarCarrinho'])->name('site.addcarrinho');
+
+Route::post('/remover', [CarrinhoController::class, 'removerCarrinho'])->name('site.removecarrinho');
+
+Route::get('/limpar', [CarrinhoController::class, 'limparCarrinho'])->name('site.limparcarrinho');
+
+Route::view('/login', 'login.form')->name('login.form');
+
+Route::post('/auth', [LoginController::class, 'auth'])->name('login.auth');
+
+Route::get('/logout', [LoginController::class, 'logout'])->name('login.logout');
+
+Route::get('/register', [LoginController::class, 'create'])->name('login.create');
+
+Route::get('/admin/dashboard', [DashboardController::class, 'index'])->name('admin.dashboard')->middleware(['auth', 'checkemail']);
+
+Route::get('/admin/produtos', [DashboardController::class, 'produtos'])->name('admin.produtos');
+
