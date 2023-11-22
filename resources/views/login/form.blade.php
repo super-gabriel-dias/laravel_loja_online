bcde46de4d9f9a8d765e727cc88f3599a2aed973
gabriel <gabrielmyaga@gmail.com>
2023-11-22 19:49:24 -0300

    feat:form for login view

---
+++ resources/views/login/form.blade.php
@@ -0,0 +1,21 @@
+@if ($mensagem = Session::get('erro'))
+    {{ $mensagem }}
+@endif
+
+@if ($errors->any())
+
+    @foreach ($errors->all() as $error)
+    {{$error}} <br>
+    @endforeach
+
+@endif
+
+<form action="{{ route('login.auth') }}" method="POST">
+    @csrf
+
+    Email: <br><input name="email"><br>
+    Senha: <br><input type="password" name="password"><br>
+    <input type="checkbox" name="remember"> Lembrar de mim
+
+    <button type="submit">Entrar</button>
+</form>
