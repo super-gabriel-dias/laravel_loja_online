f7f8489b03fcedc659a2199d2c31d9b0797ed091
gabriel <gabrielmyaga@gmail.com>
2023-11-22 19:23:50 -0300

    feat:Product model created a connection between the categories and the products within the Product model

---
+++ app/Models/Produto.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Produto extends Model
+{
+    use HasFactory;
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'id_user');
+    }
+
+    public function categoria()
+    {
+        return $this->belongsTo(Categoria::class, 'id_categoria');
+    }
+}
