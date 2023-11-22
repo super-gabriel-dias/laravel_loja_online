4c458b81eabf3933f8a7141acbde6a4ac1c83bb4
gabriel <gabrielmyaga@gmail.com>
2023-11-22 19:45:41 -0300

    feat:footer component view

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,56 @@
+<div class="wrapper" id="rodape">
+    <div id="link-laranja" class="container-fluid bg-black">
+        <footer class="row row-cols-1 row-cols-sm-2 row-cols-md-5 py-0 my-3">
+            <div class="col mb-3">
+                <a href="/" class="d-flex align-items-center mb-3 link-body-emphasis text-decoration-none"
+                    style="font-size: 200%">
+                    <strong style="color: white">Nozama</strong>
+                </a>
+                <p style="color: white">© 2023</p>
+            </div>
+            <div class="col mb-3">
+            </div>
+            <div class="col mb-3">
+                <h5 style="color: rgb(255, 85, 23)">Section</h5>
+                <ul class="nav flex-column">
+                    <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-white">Home</a></li>
+                    <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-white">Features</a>
+                    </li>
+                    <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-white">Pricing</a>
+                    </li>
+                    <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-white">FAQs</a></li>
+                    <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-white">About</a></li>
+                </ul>
+            </div>
+            <div class="col mb-3">
+                <h5 style="color: rgb(255, 85, 23)">Section</h5>
+                <ul class="nav flex-column">
+                    <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-white">Home</a></li>
+                    <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-white">Features</a>
+                    </li>
+                    <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-white">Pricing</a>
+                    </li>
+                    <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-white">FAQs</a>
+                    </li>
+                    <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-white">About</a>
+                    </li>
+                </ul>
+            </div>
+            <div class="col mb-3">
+                <h5 style="color: rgb(255, 85, 23)">Section</h5>
+                <ul class="nav flex-column">
+                    <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-white">Home</a>
+                    </li>
+                    <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-white">Features</a>
+                    </li>
+                    <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-white">Pricing</a>
+                    </li>
+                    <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-white">FAQs</a>
+                    </li>
+                    <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-white">About</a>
+                    </li>
+                </ul>
+            </div>
+        </footer>
+    </div>
+</div>
